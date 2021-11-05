72a3b6c0c01ab9266b610f9635df7432da6d844c
Agustina Edmé Aliciardi <agustina.aliciardi@claro.com.ar>
2021-11-05 09:04:27 -0300

    Add steps to check new tab and url

--- GerkingCourseTAU/tests/step_defs/test_contribute_steps.py
+++ GerkingCourseTAU/tests/step_defs/test_contribute_steps.py
@@ -18,9 +18,22 @@ def access_contactUs(browser):
 @then(parsers.parse('the title "{title}" is displayed in the new page'))
 def complete_contact_form(browser, title):
     contribute_page = ContributePage(browser)
+    # move to new tab
     browser.switch_to.window(browser.window_handles[1])
 
     assert contribute_page.getTitle().text == title
     print('Title is: ' + title)
+
+
+@then(parsers.parse('the title of the tab is "{tab_title}"'))
+def check_tab_title(browser, tab_title):
+    assert browser.title == tab_title
+    print('The title of the tab is: ' + tab_title)
+
+
+@then(parsers.parse('the url is "{url_new}"'))
+def check_new_url(browser, url_new):
+    assert browser.current_url == url_new
+    print('The url is: ' + url_new)
 
 
